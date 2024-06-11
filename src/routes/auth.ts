4c886572432c400e8f6dbcd4a120d68a0a3a325b
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-06-11 23:42:37 +0530

    feat: :fire: Added Prisma, vitest and user models in prisma

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,7 @@
 // Imports
 import { Hono } from "hono";
 
-import { emailLoginAuth } from "../schemas/auth.schema";
+import { emailLoginAuth, registrationSchema } from "../schemas/auth.zodschema";
 import { zValidator } from "@hono/zod-validator";
 import { HTTPException } from "hono/http-exception";
 
@@ -18,6 +18,27 @@ export const authRoute = new Hono()
 		if (password) {
 			throw new HTTPException(401, { message: "Unauthorized" });
 		}
-		return c.json({success: true, message: "Access Granted"});
-	});
+		return c.json({ success: true, message: "Access Granted" });
+	}).post("/register", zValidator("json", registrationSchema), async (c) => {
+		try {
+			const body = await c.req.json()
+			if (!body) throw new HTTPException(400, { message: "Invalid Data" })
+
+			// hash the password
+			const hash = await Bun.password.hash(body.password, {
+				algorithm: "bcrypt",
+				cost: 4
+			});
+			// Verify match
+			const isMatch = await Bun.password.verify(body.password, hash)
+			if (!isMatch) {
+				throw new HTTPException(500, { message: "Something went wrong" })
+			}
+			console.log(body.password, hash, isMatch)
+
+			return c.json({ success: true, message: "Registration Successful" })
+		} catch (error) {
+			return c.json({ error: true, message: "Something went wring" })
+		}
+	})
 
